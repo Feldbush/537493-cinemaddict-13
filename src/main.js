16847dea6490042b340e54e2ca330df827105f56
Feldbush <feldbush@live.ru>
2020-12-22 23:05:08 +0300

    Merge branch 'master' of github.com:htmlacademy-ecmascript/537493-cinemaddict-13 into module3-task1

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import FilmListView from './view/base-layout';
+import FilmListView from './view/filmList';
 import FilmCardView from './view/film-card';
 import FilmPopUpView from './view/film-details';
 import UserInfoView from './view/user-Info';
@@ -6,10 +6,17 @@ import MenuView from './view/menu';
 import FilterView from './view/filter';
 import ShowMoreButtonView from './view/show-more-btn';
 
-import {render} from './utils';
+import {
+  render
+} from './utils';
 
-import {COMMENTS, filmsMockData} from './mock/film';
-import {generateFilter} from "./mock/filter";
+import {
+  COMMENTS,
+  filmsMockData
+} from './mock/film';
+import {
+  generateFilter
+} from "./mock/filter";
 
 const QUANTITY_CARDS_IN_FILMS_LIST = 5;
 
@@ -30,29 +37,48 @@ render(mainElement, filterView.getElement());
 const filmListView = new FilmListView();
 render(mainElement, filmListView.getElement());
 
-const filmsList = document.querySelector(`.films-list`);
+// const filmsList = document.querySelector(`.films-list`);
 
-const filmsListContainer = document.querySelector(`.films-list__container`);
+// const filmsListContainer = document.querySelector(`.films-list__container`);
 
 const filmPopUp = new FilmPopUpView({}, COMMENTS);
-filmPopUp.getElement();
+let filmPopUpState = false;
+
+function onEscKeyDown(evt) {
+  if (evt.key === `Escape` || evt.key === `Esc`) {
+    evt.preventDefault();
+    removeFilmPopUp();
+    document.removeEventListener(`keydown`, onEscKeyDown);
+  }
+};
+
+function removeFilmPopUp() {
+  filmPopUpState = false;
+  mainElement.removeChild(filmPopUp.getElement());
+  document.body.classList.remove(`hide-overflow`);
+}
+
+let countFilmsInList = 0;
 
 function renderFilmCard(data) {
   const filmCard = new FilmCardView(data);
 
   filmCard.setPosterClickHandler((evt) => {
+    if (filmPopUpState) return;
     evt.preventDefault();
     document.body.classList.add(`hide-overflow`);
     filmPopUp.updateElement(filmCard._data);
     filmPopUp.setCrossClickHandler((e) => {
       e.preventDefault();
-      mainElement.removeChild(filmPopUp.getElement());
-      document.body.classList.remove(`hide-overflow`);
+      removeFilmPopUp();
     });
+    filmPopUpState = true;
+    document.addEventListener(`keydown`, onEscKeyDown);
     render(mainElement, filmPopUp.getElement());
   });
 
-  render(filmsListContainer, filmCard.getElement());
+  render(filmListView.getContainer(), filmCard.getElement());
+  countFilmsInList++;
 }
 
 function renderFilms() {
@@ -63,20 +89,18 @@ function renderFilms() {
 
 renderFilms();
 
-const showMoreButtonView = new ShowMoreButtonView();
-render(filmsList, showMoreButtonView.getElement());
-
-let countFilmsInList = QUANTITY_CARDS_IN_FILMS_LIST;
-
 if (filmsMockData.length > countFilmsInList) {
+  const showMoreButtonView = new ShowMoreButtonView();
+  render(filmListView.getElement(), showMoreButtonView.getElement());
+
   const showMoreBtn = showMoreButtonView.getElement();
   showMoreBtn.addEventListener(`click`, (evt) => {
     evt.preventDefault();
     filmsMockData
-    .slice(countFilmsInList, countFilmsInList + QUANTITY_CARDS_IN_FILMS_LIST)
-    .forEach((filmCard) => {
-      renderFilmCard(filmCard);
-    });
+      .slice(countFilmsInList, countFilmsInList + QUANTITY_CARDS_IN_FILMS_LIST)
+      .forEach((filmCard) => {
+        renderFilmCard(filmCard);
+      });
 
     countFilmsInList += QUANTITY_CARDS_IN_FILMS_LIST;
 
@@ -85,3 +109,5 @@ if (filmsMockData.length > countFilmsInList) {
     }
   });
 }
+
+filmListView.emptyCheck();
